901407c5a657f107ee514051b7a271af20f6acbf
John Pevien Laguindam <johnpevienwork@gmail.com>
2023-10-05 23:41:29 +0800

    Refactor page.php for improved readability and organization

--- page.php
+++ page.php
@@ -5,23 +5,40 @@ get_header();
 while (have_posts()) {
   the_post(); ?>
 
-  <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>DONT FORGET TO REPLACE ME LATER</p>
-      </div>
+<div class="page-banner">
+  <div class="page-banner__bg-image"
+    style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title">
+      <?php the_title(); ?>
+    </h1>
+    <div class="page-banner__intro">
+      <p>DONT FORGET TO REPLACE ME LATER</p>
     </div>
   </div>
+</div>
 
-  <div class="container container--narrow page-section">
+<div class="container container--narrow page-section">
+  <?php
+  $theParentId = wp_get_post_parent_id(get_the_ID());
+  echo $theParentId;
+    if ( $theParentId) { ?>
+  <div class="metabox metabox--position-up metabox--with-home-link">
+    <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentId);?>"><i class="fa fa-home"
+          aria-hidden="true"></i> Back to
+        <?php echo get_the_title( $theParentId); ?></a>
+      <span class="metabox__main">
+        <?php the_title() ?>
+      </span>
+    </p>
+  </div>
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-    </div>
+  <?php }
+    ?>
 
-    <!--
+
+
+  <!--
     <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
       <ul class="min-list">
@@ -31,11 +48,11 @@ while (have_posts()) {
     </div>
     -->
 
-    <div class="generic-content">
-      <?php the_content(); ?>
-    </div>
-
+  <div class="generic-content">
+    <?php the_content(); ?>
   </div>
+
+</div>
 
 <?php }
 
