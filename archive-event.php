abb5180085d24f539051fbdfe6e945d5200e24f0
John Pevien Laguindam <johnpevienwork@gmail.com>
2023-10-21 23:44:25 +0800

    Update post layout in archive-event.php

--- archive-event.php
+++ archive-event.php
@@ -21,19 +21,17 @@ get_header();
   <?php 
   while(have_posts()){
     the_post(); ?>
-  <div class="post-item">
-    <h2><a href="<?php the_permalink(); ?>">
-        <?php the_title(); ?>
-      </a></h2>
-    <!-- <div class="metabox">
-      <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in
-        <?php echo get_the_category_list(', '); ?> </p>
-    </div> -->
-    <div class="generic-content">
-      <?php the_excerpt();  ?>
-      <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
+  <div class="event-summary">
+    <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+      <span class="event-summary__month"><?php the_time('M') ?></span>
+      <span class="event-summary__day"><?php the_time('d')  ?></span>
+    </a>
+    <div class="event-summary__content">
+      <h5 class="event-summary__title headline headline--tiny"><a
+          href="<?php the_permalink();?>"><?php the_title() ?></a></h5>
+      <p><?php echo wp_trim_words(get_the_content(), 18);    ?><a href="<?php the_permalink() ?>" class="nu gray">Read
+          more</a></p>
     </div>
-
   </div>
 
   <?php } 
