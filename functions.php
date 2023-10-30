0a3d2502769343756425b6a1a9c94aee96d49049
John Pevien Laguindam <johnpevienwork@gmail.com>
2023-10-30 21:16:39 +0800

    Update query filter for program post type archive

--- functions.php
+++ functions.php
@@ -65,6 +65,15 @@ add_action('init', 'university_post_types');
 
 // Query filter for events archive
 function university_adjust_queries($query){
+
+  if(!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()){
+    $query->set('post_per_page', -1);
+    $query->set('orderby', 'title');
+    $query->set('order', 'ASC'); 
+  
+  }
+
+
   if(!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()){
     $query->set('meta_key', 'event_date');
     $query->set('orderby', 'meta_value_num');
