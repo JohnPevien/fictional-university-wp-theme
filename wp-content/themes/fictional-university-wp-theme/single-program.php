ba78a8894a45aaf58b8098f4709de2c8eced5ef8
John Pevien Laguindam <johnpevienwork@gmail.com>
2023-11-30 14:55:24 +0800

    Refactor single-program.php to display related professors and upcoming events

--- wp-content/themes/fictional-university-wp-theme/single-program.php
+++ wp-content/themes/fictional-university-wp-theme/single-program.php
@@ -35,14 +35,12 @@ get_header();
 
   <?php 
 
-        $homepageEvents= new WP_Query(array(
-          'posts_per_page' => 2,
-          'post_type' => 'event',
-          'meta_key' => 'event_date',
-          'orderby' => 'meta_value_num',
+  $relatedProfessors= new WP_Query(array(
+          'posts_per_page' => -1,
+          'post_type' => 'professor',
+          'orderby' => 'title',
           'order' => 'ASC',
           'meta_query' => array(
-           
             array(
               'key' => 'related_program',
               'compare' => 'LIKE',
@@ -51,13 +49,43 @@ get_header();
             )
         ));
 
-        if($homepageEvents->have_posts()){
+        if($relatedProfessors->have_posts()){
           
         echo '<hr class="section-break">';
-        echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+        echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
 
-        while($homepageEvents->have_posts()){
-          $homepageEvents->the_post()?>
+        while($relatedProfessors->have_posts()){
+          $relatedProfessors->the_post()?>
+
+  <li><a href="<?php the_permalink();?>"><?php the_title();?></a></li>
+  <?php }}
+        wp_reset_postdata();  // Cleaning up after the query
+      ?><?php
+
+  $homepageEvents= new WP_Query(array(
+  'posts_per_page' => 2,
+  'post_type' => 'event',
+  'meta_key' => 'event_date',
+  'orderby' => 'meta_value_num',
+  'order' => 'ASC',
+  'meta_query' => array(
+
+  array(
+  'key' => 'related_program',
+  'compare' => 'LIKE',
+  'value' => '"' . get_the_ID() . '"',
+  )
+  )
+  ));
+
+  if($homepageEvents->have_posts()){
+
+  echo '
+  <hr class="section-break">';
+  echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+
+  while($homepageEvents->have_posts()){
+  $homepageEvents->the_post()?>
 
   <div class="event-summary">
     <a class="event-summary__date t-center" href="#">
